6aa97b5df7be1ca7761c7852a4a95b5de11eac11
Макарова Юлия Андреевна <julmakan@gmail.com>
2023-02-23 23:15:34 +0300

    added movies_edit

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from web.views import main_view, registration_view, auth_view, logout_view, movies_add_view
+from web.views import main_view, registration_view, auth_view, logout_view, movies_edit_view
 
 urlpatterns = [
     path("", main_view, name="main"),
     path("registration/", registration_view, name="registration"),
     path("auth/", auth_view, name="auth"),
     path("logout/", logout_view, name="logout"),
-    path("movies/add/", movies_add_view, name="movies_add"),
+    path("movies/add/", movies_edit_view, name="movies_add"),
+    path("movies/<int:id>/", movies_edit_view, name="movies_edit"),
 ]
--- web/views.py
+++ web/views.py
@@ -52,8 +52,9 @@ def logout_view(request):
     return redirect('main')
 
 
-def movies_add_view(request):
-    form = MovieForm()
+def movies_edit_view(request, id=None):
+    movie = Movie.objects.get(id=id) if id is not None else None
+    form = MovieForm(instance=movie)
     if request.method == 'POST':
         form = MovieForm(data=request.POST, initial={"user": request.user})
         if form.is_valid():
