edb1703075ee83ae2101d06861bc16a64f22956a
Макарова Юлия Андреевна <julmakan@gmail.com>
2023-02-23 16:12:41 +0300

    added models

--- web/models.py
+++ web/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils import timezone
-from django.db.models import F, Sum
 
 User = get_user_model()
 
 
-class TimeSlotTag(models.Model):
+class MovieGenre(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
-    start_date = models.DateTimeField(verbose_name='Время начала', default=timezone.now)
-    end_date = models.DateTimeField(verbose_name='Время окончания', null=True, blank=True)
-    is_realtime = models.BooleanField(default=False, verbose_name='В реальном времени')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    tags = models.ManyToManyField(TimeSlotTag, verbose_name='Теги', blank=True)
-    image = models.ImageField(upload_to='time_slots/', null=True, blank=True, verbose_name='Картинка')
+    genres = models.ManyToManyField(MovieGenre, verbose_name='Жанры', blank=True)
+    image = models.ImageField(upload_to='movies/', null=True, blank=True, verbose_name='Картинка')
